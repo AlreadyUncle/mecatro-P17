457dc2b8b76346ec43d58792cbe41eaaeb2d7e00
garbadrom <33730312+garbadrom@users.noreply.github.com>
2018-10-16 16:45:48 +0200

    add an example for data output via serial port

---
+++ examples/main.cpp
@@ -0,0 +1,36 @@
+// penser a inactiver la liaison serie en retirant toute reference a la liaison dans fichier /boot/cmdline.txt
+
+
+#include <iostream>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+    int fd;
+    char caractere = 'B';
+    fd = serialOpen("/dev/serial0", 9600);
+
+    if (fd != -1) {
+        cout << fd << endl;
+
+        for (;;) {
+
+
+            //caractere = serialGetchar(fd); //reception sur liaison serie RXD0 du RPI
+
+            serialPutchar(fd, caractere); // reemettre sur TXD du RPI (mode echo)
+
+            cout << caractere << endl; // sans "endl" ca n'affiche pas sur console(ecran)
+            delay(1);
+
+        }
+
+    } else {
+        cout << fd << endl;
+        cout << "Erreur" << endl;
+    }
+    return 0;
+
+}
