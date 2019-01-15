17160fe5c6e1d72dcbf9516be2acb768a6a9ba53
alexandrethm <33730312+alexandrethm@users.noreply.github.com>
2019-01-15 16:11:22 +0100

    file structure defined

---
+++ build/README.md
@@ -0,0 +1,2 @@
+### Build repository for dynamixel_sdk library
+Don't touch ?
---
+++ example/our_examples/kangaroo/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "kangaroo.h"
+#include "serialport.h"
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+using namespace std;
+
+int main()
+{
+    
+    bool etat;
+    Ckangaroo robot("/dev/serial0");
+    etat=robot.isOperationnal();
+    if(etat==true)
+    {
+        cout<<"Operationnal"<<endl;
+        cout<<robot.forwardB(10,50,cm)<<endl;
+        //cout<<robot.turnB(500,360)<<endl;
+
+    }
+    else
+    {
+        cout<<"Connection rrror"<<endl;
+    }
+
+}
